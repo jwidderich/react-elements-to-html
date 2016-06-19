045b29da4c9756a333df0422a48b359fc6b61e85
Jannis Widderich <j.widderich@bichinger.de>
2016-06-19 13:46:20 +0200

    add babel

---
+++ spec/spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+var x = (<div>FOO</div>);
+
+var y =
+`<div>
+    FOO
+</div>`
+
+console.log(y);
+
+describe("react elements to html", () => {
+  it("is true", () => {
+    expect(true).toBe(true)
+  });
+});
